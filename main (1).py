b5167405e9fa9b40ccd775de19fc2b3bd21a245c
StasShfransky <stanislavshaf@gmail.com>
2024-09-10 09:51:05 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+stanislav = "Hello World!"
+name = "Stas"
+lastname = "Shafransky"
+age = "17"
+
+print(stanislav)
+print(name)
+print(lastname)
+print(age)
+
+print(type(name))
+print(type(lastname))
+print(type(age))
+print(type(stanislav))
